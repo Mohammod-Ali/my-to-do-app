9e16dfdd06903fb9654c76abdd05f88350f9109c
Mohammod Ali <mohammodali651@gmail.com>
2024-08-01 16:34:54 +0600

    task board added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Footer from './Footer'
 import Header from './Header'
 import HeroSection from './HeroSection'
+import TaskBoard from './task/TaskBoard'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
     <Header></Header>
     <HeroSection></HeroSection>
+    <TaskBoard></TaskBoard>
     <Footer></Footer>
     </>
   )

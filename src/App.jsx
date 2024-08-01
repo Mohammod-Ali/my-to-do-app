5305a36b80239fc9c95abbf062f068a8aab94cc2
Mohammod Ali <mohammodali651@gmail.com>
2024-08-01 15:00:33 +0600

    hero section added and dark mood added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Footer from './Footer'
 import Header from './Header'
+import HeroSection from './HeroSection'
 
 function App() {
   
@@ -8,10 +9,7 @@ function App() {
   return (
     <>
     <Header></Header>
-    
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <HeroSection></HeroSection>
     <Footer></Footer>
     </>
   )
